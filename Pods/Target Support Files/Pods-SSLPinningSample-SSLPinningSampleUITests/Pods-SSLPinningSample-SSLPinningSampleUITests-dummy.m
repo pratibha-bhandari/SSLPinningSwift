83e1776ad4c1ef34d6ac9ff0e43fd1ae343d1033
Pratibha <>
2020-12-16 21:17:50 +0530

    Initial commit

---
+++ Pods/Target Support Files/Pods-SSLPinningSample-SSLPinningSampleUITests/Pods-SSLPinningSample-SSLPinningSampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SSLPinningSample_SSLPinningSampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SSLPinningSample_SSLPinningSampleUITests
+@end
---
+++ Pods/Target Support Files/Pods-SSLPinningSample/Pods-SSLPinningSample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SSLPinningSample : NSObject
+@end
+@implementation PodsDummy_Pods_SSLPinningSample
+@end
